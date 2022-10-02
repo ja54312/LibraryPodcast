275d6da77d9227fb5ae5173d0a4cf27075b00b9d
ja54312 <ija54312@gmail.com>
2022-10-02 18:03:44 -0500

    Updates

---
+++ _next/static/WDpZVsPo725KREM6uhMJz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-905095537b2e2b9c.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
