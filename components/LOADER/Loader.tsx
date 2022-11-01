0201cbfbef59a0df724a5274ad9db8bb7dd6b9e6
ja54312 <ija54312@gmail.com>
2022-10-31 21:31:38 -0600

    ajustes llamado

--- components/LOADER/Loader.tsx
+++ components/LOADER/Loader.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div className="lds-ring">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div className='container-loader'>
+      <div className="lds-ring">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,26 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../components/HEADER/header'
 import Search from '../components/SEARCH/search'
 import Main from '../components/MAIN/main'
 import Footer from '../components/FOOTER/footer'
 import useLocalStorage from '../hooks/useLocalStorage'
-import {helpHttp} from '../helpers/helpHttp'
+import { helpHttp } from '../helpers/helpHttp'
 import Loader from '../components/LOADER/Loader'
 import Message from '../components/MESSAGE/message'
 
 const Home = () => {
   const [dataComparator, setDataComparator] = useLocalStorage('ListSongs', []);
+  //const [diaDeHoy, setDiaDeHoy] = useLocalStorage('Dia', []);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
   let url = "https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json"
+  // let Hoy = new Date().getDate()
+  // console.log(Hoy, 'hoy')
+
+  // useEffect(() => {
+  //   setDiaDeHoy(new Date().getDate())
+  // }, []);
 
   useEffect(() => {
     setLoading(true);
@@ -30,20 +37,20 @@ const Home = () => {
         setLoading(false);
       });
   }, [url]);
-  
+
   return (
     <>
-        <Header/>
-        <Search/>
-        {loading && <Loader/>}
-        {error && (
-          <Message
-            msg={`Error ${error.status}: ${error.statusText}`}
-            bgColor="#dc3545"
-          />
-        )}
-        {dataComparator !== null?<Main data={dataComparator}/>:null}
-        <Footer/>
+      <Header />
+      <Search />
+      {loading && <Loader />}
+      {error && (
+        <Message
+          msg={`Error ${error.status}: ${error.statusText}`}
+          bgColor="#dc3545"
+        />
+      )}
+      {dataComparator !== null ? <Main data={dataComparator} /> : null}
+      <Footer />
     </>
   )
 }
