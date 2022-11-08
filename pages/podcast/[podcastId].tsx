b98cb9c6c7c9e0802fb5b583e74b377522ec022b
ja54312 <ija54312@gmail.com>
2022-11-07 19:07:10 -0600

    fetch apgeid

--- pages/podcast/[podcastId].tsx
+++ pages/podcast/[podcastId].tsx
@@ -1,14 +1,47 @@
 import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
+import Loader from '../../components/LOADER/Loader'
+import Message from '../../components/MESSAGE/message'
+import { helpHttp } from '../../helpers/helpHttp'
 
 const PodcastItem = () => {
     const router = useRouter()
     const { podcastId } = router.query
+    const [data, setData] = useState();
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const url = `https://itunes.apple.com/lookup?id=${podcastId}`
 
+    useEffect(() => {
+        setLoading(true);
+        helpHttp()
+            .get(url)
+            .then((res) => {
+                if (!res.err) {
+                    setData(res);
+                    setError(null);
+                } else {
+                    setData(null);
+                    setError(res);
+                }
+                setLoading(false);
+            });
+    }, [url]);
+    console.log(data, 'data')
     return (
-        <>
-            <h2>Es la pagina de id tal:{podcastId}</h2>
-        </>
+        <section className='section-general-podcastPage'>
+            {loading && <Loader />}
+            {error && (
+                <Message
+                    msg={`Error ${error.status}: ${error.statusText}`}
+                    bgColor="#dc3545"
+                />
+            )}
+            <div className='container-general-podcastPage'>
+                <div className='container-card-general'></div>
+                <div className='container-episodes-general'></div>
+            </div>
+        </section>
     )
 }
 
